d198828b693612aae6813fe8564d428930df7769
Teodor Patsov <jarousqui@icloud.com>
2024-07-01 11:27:14 +0300

    Added backend

---
+++ fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/FitnesstrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package zetta.fitnesstrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesstrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesstrackerbackendApplication.class, args);
+	}
+
+}
---
+++ fitnesstrackerbackend/src/test/java/zetta/fitnesstrackerbackend/FitnesstrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zetta.fitnesstrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesstrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
