d35a54776ae126cffd805a468f93b5296e9d6c50
Teodor Patsov <jarousqui@icloud.com>
2024-07-01 18:24:20 +0300

    Changed frontend directory name

--- fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/controller/UserController.java
+++ fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/controller/UserController.java
@@ -1,12 +1,30 @@
 package zetta.fitnesstrackerbackend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @PostMapping("/create")
+    public ResponseEntity<String> create() {
+        return ResponseEntity.ok("Create User");
+    }
 
+    @GetMapping("/find") // aint working
+    public ResponseEntity<String> find() {
+        return ResponseEntity.ok("Find User, id = ");
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<String> update(@PathVariable String id) {
+        return ResponseEntity.ok("Update User, id = " + id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable String id) {
+        return ResponseEntity.ok("Delete User, id = " + id);
+    }
 
 }
