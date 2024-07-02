71f9a73ddc72730365a4bdd403234219f0e3d067
GeorgiD31 <101389914+GeorgiD31@users.noreply.github.com>
2024-07-02 13:14:08 +0300

    Update Navbar.tsx

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,13 +1,15 @@
-import {useState} from "react";
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Modal from "./Modal.tsx";
+import Modal from './Modal';
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ isSignedIn, setIsSignedIn }) => {
   const [showModal, setShowModal] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
+  const toggleDropdown = () => setShowDropdown(!showDropdown);
 
   return (
     <>
@@ -17,10 +19,25 @@ const Navbar = () => {
           <Link to="/exercise">Exercise</Link>
         </div>
         <div className="navbar-right">
-          <button className="sign-in-button" onClick={handleShow}>Sign In</button>
+          {isSignedIn ? (
+            <div className="account">
+              <button className="account-button" onClick={toggleDropdown}>
+                <img src="src\images\image.jpg" alt="Profile" className="profile-pic" />
+              </button>
+              {showDropdown && (
+                <div className="dropdown-menu">
+                  <p>Username: Smetkata</p>
+                  <p>Email: andonov@gmail.com</p>
+                  <button onClick={() => setIsSignedIn(false)}>Sign Out</button>
+                </div>
+              )}
+            </div>
+          ) : (
+            <button className="sign-in-button" onClick={handleShow}>Sign In</button>
+          )}
         </div>
       </nav>
-      <Modal show={showModal} handleClose={handleClose} />
+      <Modal show={showModal} handleClose={handleClose} setIsSignedIn={setIsSignedIn} />
     </>
   );
 };
