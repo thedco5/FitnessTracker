d9233ecf8e11cd75b3c8a2ed3584bbddab8ce5a2
Teodor Patsov <jarousqui@icloud.com>
2024-07-03 11:20:19 +0300

    Added mapper skeletons

---
+++ fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package zetta.fitnesstrackerbackend.mapper;
+
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    
+
+}
