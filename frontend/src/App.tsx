1d03d8109c45a5fdb0b684555660fd08db4d65e9
Alexei <alex.skorokhod06@gmail.com>
2024-07-03 21:15:00 +0300

    add workouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Navbar from './Navbar';
 import { Exercises } from "./features/exercises";
 import { Workouts } from "./features/workouts";
+import { Workout } from "./features/workouts/Workout";
 
 const Home = () => <div>Home Page</div>;
 
@@ -10,16 +11,17 @@ function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
-    <Router>
-      <div>
-        <Navbar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/exercises" element={<Exercises isSignedIn={isSignedIn} />} />
-          <Route path="/workouts" element={<Workouts />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Navbar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercises" element={<Exercises isSignedIn={isSignedIn} />} />
+            <Route path="/workouts" element={<Workouts isSignedIn={isSignedIn} />} />
+            <Route path="/workout/:workoutId" element={<Workout />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
