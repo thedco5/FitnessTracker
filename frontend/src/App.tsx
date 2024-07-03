bb9ae13ae01052b6c4aa33a1fd56d868ae70d2d0
GeorgiD31 <101389914+GeorgiD31@users.noreply.github.com>
2024-07-03 14:00:34 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Navbar from './Navbar';
-import {Exercises} from "./features/exercises";
-import {Workouts} from "./features/workouts";
+import { Exercises } from "./features/exercises";
+import { Workouts } from "./features/workouts";
 
 const Home = () => <div>Home Page</div>;
 
@@ -15,8 +15,8 @@ function App() {
         <Navbar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
         <Routes>
           <Route path="/" element={<Home />} />
-           <Route path="/exercises" element={<Exercises />} />
-           <Route path="/workouts" element={<Workouts />} />
+          <Route path="/exercises" element={<Exercises isSignedIn={isSignedIn} />} />
+          <Route path="/workouts" element={<Workouts />} />
         </Routes>
       </div>
     </Router>
