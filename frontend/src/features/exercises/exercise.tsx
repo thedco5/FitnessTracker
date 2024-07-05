da2c771814a2b3469af051795464bd91164bbc58
GeorgiD31 <101389914+GeorgiD31@users.noreply.github.com>
2024-07-05 10:50:48 +0300

    Update exercise.tsx

--- frontend/src/features/exercises/exercise.tsx
+++ frontend/src/features/exercises/exercise.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import type { Exercise as ExerciseType } from './types';
 import './card.css';
@@ -14,13 +13,13 @@ interface ExtendedExerciseType extends ExerciseType {
 export const ExerciseCard: React.FC<ExtendedExerciseType> = ({ id, name, description, image, calories, duration, durationType, difficulty, likes }) => {
     return (
         <div className="ExerciseCard">
-            <img src={image} alt={description} className="ExerciseCard-image" />
+            <img src={image ?? undefined} alt={description} className="ExerciseCard-image" />
             <div className="ExerciseCard-info">
                 <h3 className="ExerciseCard-title">{name}</h3>
                 <p className="ExerciseCard-description">{description}</p>
             </div>
             <div className="ExerciseCard-details">
-                <p><strong>Likes:</strong> {likes}</p> {}
+                <p><strong>Likes:</strong> {likes}</p>
                 <p><strong>Calories:</strong> {calories}</p>
                 <p><strong>Duration:</strong> {duration} {durationType}</p>
                 <p><strong>Difficulty:</strong> {difficulty}</p>
