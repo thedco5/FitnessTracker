63b1a182929ef56bae941796b981329e8753d942
Teodor Patsov <jarousqui@icloud.com>
2024-07-09 21:44:06 +0300

    Docker update

--- fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/config/OpenApiConfig.java
+++ fitnesstrackerbackend/src/main/java/zetta/fitnesstrackerbackend/config/OpenApiConfig.java
@@ -4,14 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact,
-                description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification",
+                description = "ZettaFit documentation of the Spring Boot backend API",
+                title = "ZettaFit RESTful API specification",
                 version = "v0.0"
         ),
         servers = {
@@ -22,5 +27,13 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 public class OpenApiConfig {
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .components(new Components().addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                        .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+        }
 
 }
