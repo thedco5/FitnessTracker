8eab94d2f60e7b50ba3d0fa6159b0e23e876935a
GeorgiD31 <101389914+GeorgiD31@users.noreply.github.com>
2024-07-09 14:07:22 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+// App.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Navbar from './Navbar';
@@ -9,19 +10,24 @@ const Home = () => <div>Home Page</div>;
 
 function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState<{ username: string; email: string; image: string | null }>({
+    username: '',
+    email: '',
+    image: null,
+  });
 
   return (
-      <Router>
-        <div>
-          <Navbar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/exercises" element={<Exercises isSignedIn={isSignedIn} />} />
-            <Route path="/workouts" element={<Workouts isSignedIn={isSignedIn} />} />
-            <Route path="/workout/:workoutId" element={<Workout />} />
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Navbar isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} userInfo={userInfo} setUserInfo={setUserInfo} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercises" element={<Exercises isSignedIn={isSignedIn} />} />
+          <Route path="/workouts" element={<Workouts isSignedIn={isSignedIn} showAddModal={undefined} />} />
+          <Route path="/workout/:workoutId" element={<Workout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
